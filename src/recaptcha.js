7d719f28d8b7f73e7671a404b98245488871b7c1
Dragos Cirjan <dragos.cirjan@itmediaconnect.ro>
2016-02-23 00:16:54 +0200

    tests fix

--- src/recaptcha.js
+++ src/recaptcha.js
@@ -12,12 +12,10 @@ import {inject, noView, bindable} from 'aurelia-framework';
 const recaptchaCallbackName = 'setRecaptchaReady';
 const ready = new Promise(resolve => window[recaptchaCallbackName] = resolve);
 
-// https://developers.google.com/recaptcha/docs/display#explicit_render
 /**
- * [createElement description]
- * @method createElement
- * @param  {[type]}      'script' [description]
- * @return {[type]}               [description]
+ * Google Recaptcha Script
+ * @var {DocumentElement}
+ * @link https://developers.google.com/recaptcha/docs/display
  */
 let script = document.createElement('script');
 script.src = `https://www.google.com/recaptcha/api.js?onload=${recaptchaCallbackName}&render=explicit&hl=${document.getElementsByTagName('html')[0].getAttribute('lang') || 'en'}`;
@@ -25,6 +23,16 @@ script.async = true;
 script.defer = true;
 document.head.appendChild(script);
 
+/**
+ * Google Recaptcha plugin, originally developed by Jeremy Danyow (http://stackoverflow.com/users/725866/jeremy-danyow)
+ * and extended by Dragos Cirjan (http://github.com/dragoscirjan)
+ *
+ * @link http://stackoverflow.com/questions/35441787/use-googles-recaptcha-in-an-aurelia-application
+ * @link http://plnkr.co/edit/rXqNdh?p=info
+ *
+ * @method noView
+ * @method inject
+ */
 @noView()
 @inject(Element)
 export class Recaptcha {
@@ -36,10 +44,20 @@ export class Recaptcha {
     @bindable type = 'image';
     @bindable sitekey = '';
 
+    /**
+     * [constructor description]
+     * @method constructor
+     * @param  {DocumentElement}    element Element to render recaptcha
+     * @return {this}
+     */
     constructor(element) {
         this.element = element;
     }
 
+    /**
+     * Attached event (see Aurelia Component Documentation)
+     * @method attached
+     */
     attached() {
         ready.then(() => {
             grecaptcha.render(this.element, {
